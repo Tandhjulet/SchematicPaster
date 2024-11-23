cecddb22e56ab5de8ef66a27ab8a183cb2f08764
Tandhjulet <madsbechmortensen@hotmail.dk>
2024-11-23 19:05:34 +0100

    simplified config

--- src/main/java/dk/tandhjulet/config/Config.java
+++ src/main/java/dk/tandhjulet/config/Config.java
@@ -1,9 +1,5 @@
 package dk.tandhjulet.config;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
 
@@ -18,18 +14,6 @@ public class Config extends OkaeriConfig {
 	@Comment({ "Origin to place the map at. If the schematic origin is at",
 			"spawn, this location should be at spawn aswell." })
 	private Location mapOrigin = new Location(defaultWorld, 0, 0, 0);
-
-	@Getter
-	@Comment("Define what maps that should be in rotation here.")
-	private List<MapConfig> maps = new ArrayList<>(Collections.singleton(new MapConfig()));
-
-	public class MapConfig extends OkaeriConfig {
-		@Comment("Map name")
-		private String name = "Test map";
-
-		@Comment("Path to schematic file")
-		private String schemFile = "test.schem";
-	}
 
 	@Getter
 	@Comment("Max chunks to place pr. second. Set to -1 to disable.")
--- src/main/java/dk/tandhjulet/map/MapManager.java
+++ src/main/java/dk/tandhjulet/map/MapManager.java
@@ -2,17 +2,13 @@ package dk.tandhjulet.map;
 
 import java.io.File;
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
 
 import org.bukkit.Bukkit;
 
 import dk.tandhjulet.SchematicPaster;
-import dk.tandhjulet.config.Config.MapConfig;
 
 public class MapManager {
-	private HashMap<String, Schematic> maps = new HashMap<>();
-	private ArrayList<String> mapNames = new ArrayList<>();
+	private ArrayList<Schematic> maps = new ArrayList<>();
 
 	private static MapManager instance;
 	private static File schematicDirectory = new File(SchematicPaster.getPlugin().getDataFolder(), "schematics");
@@ -20,40 +16,18 @@ public class MapManager {
 	private MapManager() {
 		instance = this;
 
-		List<MapConfig> registeredMaps = SchematicPaster.getConf().getMaps();
-
-		if (registeredMaps == null)
-			return;
-
-		registeredMaps.forEach((map) -> {
-			String filePath = map.get("schemFile", String.class);
-
+		for (File schematicFile : schematicDirectory.listFiles()) {
 			try {
-				String mapName = map.get("name", String.class);
-
-				File schemFile = new File(schematicDirectory, filePath);
-				if (!schemFile.exists()) {
-					Bukkit.getLogger().severe("File " + filePath + " doesn't exist.");
-					return;
-				}
-
-				Schematic registered = new SchematicReader(schemFile).getSchematic();
-				maps.put(mapName, registered);
-				mapNames.add(mapName);
+				Schematic registered = new SchematicReader(schematicFile).getSchematic();
+				maps.add(registered);
 			} catch (Exception e) {
 				e.printStackTrace();
 			}
-
-		});
+		}
 	}
 
 	public void pasteMap(int index) {
-		String mapName = mapNames.get(index);
-		pasteMap(mapName);
-	}
-
-	public void pasteMap(String name) {
-		Schematic map = maps.get(name);
+		Schematic map = maps.get(index);
 		map.load();
 		Bukkit.getLogger().info("[SchematicPaster] Pasting...");
 		map.paste();
